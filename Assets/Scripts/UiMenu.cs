ab06c24a2062bfe518c9a8c77e3d6d8c314792eb
Gustavo Lucas da Rosa <gustavolucasdarosa@gmail.com>
2022-11-07 20:42:06 -0300

    Implemented PausedGame

--- Assets/Scripts/UiMenu.cs
+++ Assets/Scripts/UiMenu.cs
@@ -8,10 +8,23 @@ public class UiMenu : MonoBehaviour
 {
     public GameObject EndingDialog;
     public GameObject InGameInfoDialog;
+    public GameObject PauseDialog;
 
     private void Start()
     {
         GameManager.Instance.Init(EndingDialog, InGameInfoDialog);
+    }
+
+    public void Pause()
+    {
+        Time.timeScale = 0f;
+        PauseDialog.SetActive(true);
+    }
+
+    public void Resume()
+    {
+        Time.timeScale = 1f;
+        PauseDialog.SetActive(false);
     }
 }
 
